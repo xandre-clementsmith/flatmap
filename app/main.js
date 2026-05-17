58e6822fbbb5fcd74542de8598b4ac70a1c4c418
Xandre Clementsmith <xandre.clementsmith@gmail.com>
2026-05-17 07:55:02 -0400

    App add-5. Fixed collapsing window.

--- app/main.js
+++ app/main.js
@@ -866,6 +866,7 @@ async function main() {
       deselect(); clearInfo();
       if (appMode === 'networks') netSelect(null);
       else if (isMobileLayout()) closeMobilePanel();
+      if (isMobileLayout()) closeMobilePathway();
     }
   });
 
